70210eedc06e7607e33bd85b5ef856671ba86f56
Seth Farrar <seth@ai-sys.com>
2017-11-13 12:16:16 -0600

    Updates maps api to current version

---
+++ index.js
@@ -0,0 +1,91 @@
+function initMap() {
+	var pSlides = [
+		['slide1.jpg', 35.1966130, -97.4822960 ],
+		['slide2.jpg', 35.2349110, -97.3957860 ],
+		['slide3.jpg', 35.2346300, -97.5255620 ],
+		['slide4.jpg', 35.2297253, -97.3233108 ],
+		['slide5.jpg', 35.2563629, -97.4599533 ],
+		['slide6.jpg', 35.1992917, -97.4010734 ],
+		['slide7.jpg', 35.2018867, -97.4314575 ],
+		['slide8.jpg', 35.2104426, -97.4653606 ]
+	];
+
+	var swLatLng = {lat:pSlides[0][1], lng:pSlides[0][2]};
+	var neLatLng = {lat:pSlides[0][1], lng:pSlides[0][2]};
+
+	for(var i=1; i<pSlides.length; ++i){
+		var el = pSlides[i];
+		if(el[1] < swLatLng.lat){ swLatLng.lat = el[1]; }
+		if(el[2] < swLatLng.lng){ swLatLng.lng = el[2]; }
+		if(el[1] > neLatLng.lat){ neLatLng.lat = el[1]; }
+		if(el[2] > neLatLng.lng){ neLatLng.lng = el[2]; }
+	}
+
+	var activeSlide = {
+		index: 0,
+		el: null,
+	};
+	var latofset = 0.006;
+	var lngofset = 0.012;
+	var bgofset = 0.0005;
+	var map;
+	var myzoom = 16;
+	var nextslide = [];
+
+	function initialize() {
+		// Setup all our google maps components
+		var newark = new google.maps.LatLng(35.196613, -97.482296);
+		var geocoder = new google.maps.Geocoder();
+		var myOptions = {
+			zoom: myzoom,
+			center: newark,
+			mapTypeId: google.maps.MapTypeId.SATELLITE,
+			disableDoubleClickZoom: true
+		}
+
+		map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+		var row;
+		for (row = 0; row < pSlides.length; ++row) {
+
+			var nlat = pSlides[row][1];
+			var nlng = pSlides[row][2];
+			var nrow = row + 1;
+
+			if(nrow == pSlides.length) { nrow = 0; }
+
+			var imageBounds = new google.maps.LatLngBounds(
+				new google.maps.LatLng(parseFloat(nlat - latofset), parseFloat(nlng - lngofset)),
+				new google.maps.LatLng(parseFloat(nlat + latofset), parseFloat(nlng + lngofset)) );
+
+			var bgBounds = new google.maps.LatLngBounds(
+				new google.maps.LatLng(parseFloat(nlat - latofset - bgofset), parseFloat(nlng - lngofset - bgofset)),
+				new google.maps.LatLng(parseFloat(nlat + latofset + bgofset), parseFloat(nlng + lngofset + bgofset)) );
+
+			var bgOverlay = new ProjectedOverlay(bgBounds, "background.png", map); // adds bg image behind slides
+			var slideOverlay = new google.maps.GroundOverlay( pSlides[row][0], imageBounds );
+			slideOverlay.setMap(map);
+			//var slideOverlay = new ProjectedOverlay(imageBounds, pSlides[row][0], map, {isClickable:true});
+			linkSlide(slideOverlay, nrow);
+
+
+		}
+
+		function linkSlide(curSlide, nextIndex) {
+
+			var nxtlat =  pSlides[nextIndex][1];
+			var nxtlng =  pSlides[nextIndex][2];
+
+			var nextSlide = new google.maps.LatLng( parseFloat(nxtlat), parseFloat(nxtlng) );
+			//curSlide.setNextSlide(nextSlide);
+
+			google.maps.event.addListener(curSlide, "click", function(){
+				map.setCenter(nextSlide);
+				map.setZoom(16);
+			});
+		}
+
+	}
+	
+	window.onload = initialize;
+}
--- projectedoverlay.js
+++ projectedoverlay.js
@@ -1,134 +1,108 @@
-/**
-  Create an overlay on the map from a projected image...
-  Author. John D. Coryat 01/2008
-  USNaviguide LLC - http://www.usnaviguide.com
-  Thanks go to Mile Williams EInsert: http://econym.googlepages.com/einsert.js, Google's GOverlay Example and Bratliff's suggestion...
-  Opacity code from TPhoto: http://gmaps.tommangan.us/addtphoto.html
-
-  Parameters: 
-    imageUrl: URL of the image
-    bounds: Bounds object of image destination
-    addZoom: Added Zoom factor as a parameter to the imageUrl (include complete parameter, including separater like '?zoom='
-
-  Licensed under the Apache License, Version 2.0;
-  http://www.apache.org/licenses/LICENSE-2.0
-*/
-
-function ProjectedOverlay(imageUrl, bounds, addZoom)
-{
- this.url_ = imageUrl ;
- this.bounds_ = bounds ;
- this.addZ_ = addZoom ;                         // Add the zoom to the image as a parameter
-
- // Is this IE, if so we need to use AlphaImageLoader
-
- var agent = navigator.userAgent.toLowerCase();
- if ((agent.indexOf("msie") > -1) && (agent.indexOf("opera") < 1))
- {
-  this.ie = true ;
- } else
- {
-  this.ie = false ;
- }
- this.ie = false ;
-}
-
 ProjectedOverlay.prototype = new google.maps.OverlayView();
 
-ProjectedOverlay.prototype.initialize = function(map)
-{
- var div = document.createElement("div") ;
- div.style.position = "absolute" ;
- div.setAttribute('id',this.id) ;
- map.getPane(G_MAP_MAP_PANE).appendChild(div) ;
- this.map_ = map ;
- this.div_ = div ;
- if( this.percentOpacity )
- {
-  this.setOpacity(this.percentOpacity) ;
- }
+function ProjectedOverlay(bounds, image, map, options) {
+	// Initialize properties
+	this.bounds_ = bounds;
+	this.image_ = image;
+	this.map_ = map;
+	this.options = options || {isClickable:false};
+	this.nextSlide_ = null;
+
+	// Define a property to hold the image's div. We'll
+	// actually create this div upon receipt of the onAdd()
+	// method so we'll leave it null for now
+	this.div_ = null;
+
+	// Expclicitly call setMap
+	this.setMap(map);
 }
 
-// Remove the main DIV from the map pane
+// Called with the map's panes are ready and the overlay has been added to the map
+ProjectedOverlay.prototype.onAdd = function() {
+	var div = document.createElement("div");
+	div.style.borderStyle = "none";
+	div.style.borderWidth = "0px";
+	div.style.position = "absolute";
 
-ProjectedOverlay.prototype.remove = function()
-{
- this.div_.parentNode.removeChild(this.div_);
- delete(this.map) ;
- delete(this.div) ;
+	// Create the image element and attch it to the div
+	var img = document.createElement("img");
+	img.src = this.image_;
+	img.style.width = "100%";
+	img.style.height = "100%";
+	img.style.position = "absolute";
+	div.appendChild(img);
+
+	this.div_ = div;
+
+	// Add the element to the correct pane
+	if(this.options.isClickable){
+		this.getPanes().overlayMouseTarget.appendChild(div);
+	}else{
+		this.getPanes().overlayLayer.appendChild(div);
+	}
+
+	//if(this.nextSlide_){
+	//	var self = this;
+	//	google.maps.event.addDomListener(self.div_, "click", function(){
+	//		self.map_.setCenter(self.nextSlide_);
+	//		self.map_.setZoom(16);
+	//	});
+	//}
 }
 
-// Copy our data to a new ProjectedOverlay...
+ProjectedOverlay.prototype.draw = function() {
+	// We use the SW and NE coordinates of the overlay to peg it to the correct
+	// position and size. To do this, we need to retrieve the projection from the overlay
+	var overlayProjection = this.getProjection();
 
-ProjectedOverlay.prototype.copy = function()
-{
- return new ProjectedOverlay(this.url_, this.bounds_, this.addZ_);
+	// Retrieve the SW and NE coordinates of this overlay in LatLngs and convert them to pixel
+	// coordinates. We'll use these coordinates to resize the div.
+	var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+	var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+
+	// Resize the image's div to fit the indicated dimensions
+	var div = this.div_;
+	div.style.left = sw.x + "px";
+	div.style.top = ne.y + "px";
+	div.style.width = (ne.x - sw.x) + "px";
+	div.style.height = (sw.y - ne.y) + "px";
 }
 
-// Redraw based on the current projection and zoom level...
-
-ProjectedOverlay.prototype.redraw = function(force)
-{
- // We only need to redraw if the coordinate system has changed
- if (!force)
- {
-  return ;
- }
-
- var c1 = this.map_.fromLatLngToDivPixel(this.bounds_.getSouthWest());
- var c2 = this.map_.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-
- // Now position our DIV based on the DIV coordinates of our bounds
-
- this.div_.style.width = Math.abs(c2.x - c1.x) + "px";
- this.div_.style.height = Math.abs(c2.y - c1.y) + "px";
- this.div_.style.left = Math.min(c2.x, c1.x) + "px";
- this.div_.style.top = Math.min(c2.y, c1.y) + "px";
- 
- var url = this.url_ ;
-
- if ( this.addZ_ )
- {
-  url += this.addZ_ + this.map_.getZoom() ;
- }
-
- if (this.ie)
- {
-  var loader = "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + url + "', sizingMethod='scale');" ;
-  this.div_.innerHTML = '<div style="height:' + this.div_.style.height + 'px; width:' + this.div_.style.width + 'px; ' + loader + '" ></div>' ;
- } else
- {
-  this.div_.innerHTML = '<img src="' + url + '"  width=' + this.div_.style.width + ' height=' + this.div_.style.height + ' >' ;
- }
+ProjectedOverlay.prototype.onRemove = function() {
+	this.div_.parentNode.removeChild(this.div_);
+	this.div_ = null;
 }
 
-ProjectedOverlay.prototype.setOpacity=function(opacity)
-{
- if (opacity < 0)
- {
-  opacity = 0 ;
- }
- if(opacity > 100)
- {
-  opacity = 100 ;
- }
- var c = opacity/100 ;
- var d = document.getElementById( this.id ) ;
+ProjectedOverlay.prototype.hide = function() {
+	if(this.div_){
+		this.div_.style.visibility = "hidden";
+	}
+}
 
- if (typeof(d.style.filter) =='string')
- {
-  d.style.filter = 'alpha(opacity:' + opacity + ')' ;
- }
- if (typeof(d.style.KHTMLOpacity) == 'string' )
- {
-  d.style.KHTMLOpacity = c ;
- }
- if (typeof(d.style.MozOpacity) == 'string')
- {
-  d.style.MozOpacity = c ;
- }
- if (typeof(d.style.opacity) == 'string')
- {
-  d.style.opacity = c ;
- }
+ProjectedOverlay.prototype.show = function() {
+	if(this.div_){
+		this.div_.style.visibility = "visible";
+	}
+}
+
+ProjectedOverlay.prototype.toggle = function() {
+	if(this.div_){
+		if(this.div_.style.visibility==="hidden"){
+			this.show();
+		}else{
+			this.hide();
+		}
+	}
+}
+
+ProjectedOverlay.prototype.setNextSlide = function(nextSlide){
+	this.nextSlide_ = nextSlide;
+}
+
+ProjectedOverlay.prototype.getNextSlide = function(){
+	return this.nextSlide_;
+}
+
+ProjectedOverlay.prototype.getDiv = function(){
+	return this.div_;
 }
